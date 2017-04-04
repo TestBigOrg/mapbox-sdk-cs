49fa56c9fb445b199905926e06eadd9f0b769948
David Rhodes <david.rhodes@mapbox.com>
2017-04-04 11:01:02 -0600

    Sample code and document formatting Public docs test [publish docs]

--- src/Map/RasterTile.cs
+++ src/Map/RasterTile.cs
@@ -4,40 +4,57 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
-using System;
-
-namespace Mapbox.Map {
-
-
+namespace Mapbox.Map
+{
 	/// <summary>
-	///    A raster tile from the Mapbox Style API, a encoded image representing a geographic
-	///    bounding box. Usually JPEG or PNG encoded.
+	/// A raster tile from the Mapbox Style API, an encoded image representing a geographic
+	/// bounding box. Usually JPEG or PNG encoded.
+	/// <example>
+	/// How to use 'RasterTile':
+	/// <code>
+	/// var parameters = new Tile.Parameters();
+	/// parameters.Fs = MapboxAccess.Instance;
+	/// parameters.Id = new CanonicalTileId(_zoom, _tileCoorindateX, _tileCoordinateY);
+	/// parameters.MapId = "mapbox://styles/mapbox/satellite-v9";
+	/// var rasterTile = new RasterTile();
+	/// rasterTile.Initialize(parameters, (Action)(() =>
+	/// {
+	/// 	if (string.IsNullOrEmpty(rasterTile.Error))
+	/// 	{
+	///			return;
+	///		}
+	///		var texture = new Texture2D(0, 0);
+	///		texture.LoadImage(rasterTile.Data);
+	///		_sampleMaterial.mainTexture = texture;
+	///	}));
+	/// </code>
+	/// </example>
 	/// </summary>
-	public class RasterTile : Tile {
-
+	public class RasterTile : Tile
+	{
 		private byte[] data;
 
 		/// <summary> Gets the raster tile raw data. </summary>
 		/// <value> The raw data, usually an encoded JPEG or PNG. </value>
-		public byte[] Data {
-			get {
+		public byte[] Data
+		{
+			get
+			{
 				return this.data;
 			}
 		}
 
-
-		internal override TileResource MakeTileResource(string styleUrl) {
+		internal override TileResource MakeTileResource(string styleUrl)
+		{
 			return TileResource.MakeRaster(Id, styleUrl);
 		}
 
-
-		internal override bool ParseTileData(byte[] data) {
+		internal override bool ParseTileData(byte[] data)
+		{
 			// We do not parse raster tiles as they are
 			this.data = data;
 
 			return true;
 		}
-
-
 	}
 }
